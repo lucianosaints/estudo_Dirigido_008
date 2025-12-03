5b2f3c09d26371f8d736386d0573d54e84dded75
lucianosaints <lucianosaints@gmail.com>
2025-12-02 21:28:47 -0300

    setup_initial_patr_tech

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Unidade,Sala,Status,Bem
+
+@admin.register(Unidade)
+
+class UnidadeAdmin(admin.ModelAdmin):
+    list_display = ("id","nome","enedereco")
+    search_fields = ("nome",)
+
+@admin.register(Sala)
+class UnidadeAdmin(admin.ModelAdmin):
+    list_display = ("id")
+
+
+
 # Register your models here.
